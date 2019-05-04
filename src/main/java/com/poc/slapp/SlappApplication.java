40c4d618033abb4b16bcb22c437e1f9e6acfc88c
SushilSingh10 <sushil.k.singh1010@gmail.com>
2019-05-05 00:36:19 +0530

    initial commit

---
+++ src/main/java/com/poc/slapp/SlappApplication.java
@@ -0,0 +1,13 @@
+package com.poc.slapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlappApplication.class, args);
+	}
+
+}
